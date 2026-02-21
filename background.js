5df77d8b79aca55b4d6da238b9a87ad0ccbf9637
jmkiii <36246226+jmkiii@users.noreply.github.com>
2026-02-21 15:31:20 -0600

    Rename: replace 'Lens' with 'Annote' in background.js context menu

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 // Create right-click context menu item
 browser.contextMenus.create({
-  id: "add-lens-annotation",
-  title: "📌 Add Lens Annotation Here",
+  id: "add-annote-annotation",
+  title: "📌 Add Annote Annotation Here",
   contexts: ["all"]
 });
 
-// When user clicks the context menu item, tell content script to open annotation modal
+// When user clicks the context menu item, tell content script to open coordinate annotation
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "add-lens-annotation") {
+  if (info.menuItemId === "add-annote-annotation") {
     browser.tabs.sendMessage(tab.id, {
       type: "OPEN_COORDINATE_ANNOTATION"
     });
